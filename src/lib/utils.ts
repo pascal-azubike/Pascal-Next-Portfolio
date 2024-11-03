0af59eb0857f5bbdeb87fcc028a7282a6ffc077d
pascal <pascalazubike004@gmail.com>
2024-11-03 01:49:01 -0700

    added comment in css claass merge function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// used to merge css classes
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
