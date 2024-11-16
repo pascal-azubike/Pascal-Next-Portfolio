ab7f316f3b9ef11f56b0625317ba2ead04eabbfd
pascal <pascalazubike004@gmail.com>
2024-11-16 07:21:30 -0800

    added favicon

--- generate-favicons.js
+++ generate-favicons.js
@@ -4,15 +4,25 @@ const path = require('path');
 
 // Directory setup
 const FAVICON_DIR = path.join(__dirname, 'public', 'favicon');
-if (!fs.existsSync(FAVICON_DIR)){
+if (!fs.existsSync(FAVICON_DIR)) {
     fs.mkdirSync(FAVICON_DIR, { recursive: true });
 }
 
-// Save the SVG content
+// SVG content with exact matching design
 const svgContent = `<svg width="512" height="512" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <!-- Background -->
   <rect width="512" height="512" rx="64" fill="#18181b"/>
-  <path d="M170 416L256 160L342 416H300L256 288L212 416H170Z" fill="#06b6d4"/>
-  <path d="M256 160H342C364 160 382 178 382 200V276C382 298 364 316 342 316H256V160ZM298 276H340V200H298V276Z" fill="#06b6d4" fillOpacity="0.8"/>
+  
+  <!-- Letter A - Complete capital A -->
+  <path d="
+    M180 416L256 160L332 416H300L256 256L212 416H180Z
+    M212 320H300L288 352H224L212 320Z" 
+    fill="#06b6d4"/>
+  
+  <!-- Letter P - Complete capital P -->
+  <path d="
+    M332 160V416H364V320H400C440 320 472 288 472 240C472 192 440 160 400 160H332ZM364 192H400C422 192 440 210 440 240C440 270 422 288 400 288H364V192Z"
+    fill="#06b6d4"/>
 </svg>`;
 
 fs.writeFileSync(path.join(FAVICON_DIR, 'favicon.svg'), svgContent);
