3d7191369142e35a17c207afb3233c1a8457cc65
pascal <pascalazubike004@gmail.com>
2024-11-21 05:35:29 -0800

    file upload fixed

--- src/components/admin-panel/file-upload.tsx
+++ src/components/admin-panel/file-upload.tsx
@@ -1,1 +1,14 @@
-"use client";import React, { useState } from "react";import { FileUpload } from "../ui/file-upload";export function FileUploadDemo() {    const [files, setFiles] = useState<File[]>([]);    const handleFileUpload = (files: File[]) => {        setFiles(files);    };    return (        <div className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-black border-neutral-800 rounded-lg">            <FileUpload onChange={handleFileUpload} />        </div>    );}
+"use client";
+import React, { useState } from "react";
+import { FileUpload } from "../ui/file-upload";
+export function FileUploadDemo() {
+    const [files, setFiles] = useState<File[]>([]);
+    const handleFileUpload = (files: File[]) => {
+        setFiles(files);
+    };
+    return (
+        <div className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-black border-neutral-800 rounded-lg">
+            <FileUpload onChange={handleFileUpload} />
+        </div>
+    );
+}
